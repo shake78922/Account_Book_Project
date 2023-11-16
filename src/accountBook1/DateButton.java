f6256d584110a5b510dcebdf8be4f383fd5b8f04
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2023-11-16 20:06:29 +0900

    initial gaza

---
+++ src/accountBook1/DateButton.java
@@ -0,0 +1,47 @@
+package accountBook1;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class DateButton extends JFrame implements MouseListener{
+	JButton jb = new JButton();
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		jb.setBackground(Color.pink);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		jb = (JButton)e.getSource();
+		jb.setBackground(Color.pink);
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		jb = (JButton)e.getSource();
+		jb.setBackground(Color.white);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		jb = (JButton)e.getSource();
+		jb.setBackground(Color.white);
+		
+	}
+	
+}
