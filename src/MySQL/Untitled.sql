f1dab6a938102b94d724b5117f7cbb3525ad22cb
Seungwan Yu <dbtmddhks@gmail.com>
2023-11-20 04:08:38 +0900

    총 금액 계산하는 코드만 남았다....화이팅

--- src/MySQL/Untitled.sql
+++ src/MySQL/Untitled.sql
@@ -21,9 +21,35 @@ create table accounts(
 CREATE TRIGGER add_account_after_insert
 AFTER INSERT ON account_book
 FOR EACH ROW
-INSERT INTO accounts (cash, account1, account2, account3, ID)
-VALUES (0, 0, 0, 0, NEW.ID);
+INSERT INTO accounts (cash, account1,ID)
+VALUES (0, 0, NEW.ID); 
+
+CREATE TABLE deposits (
+    deposit_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(8) NOT NULL,
+    deposit_date DATE NOT NULL,
+    deposit_amount INT NOT NULL,
+    deposit_type VARCHAR(50) NOT NULL,
+    payment_type VARCHAR(20), -- Add a new column for payment type
+    description VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES account_book(ID) ON DELETE CASCADE
+);
+
+CREATE TABLE expenses (
+    expense_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(8) NOT NULL,
+    expense_date DATE NOT NULL,
+    expense_amount INT NOT NULL,
+    expense_type VARCHAR(50) NOT NULL,
+    payment_type VARCHAR(20),
+    description VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES account_book(ID) ON DELETE CASCADE
+);
+
 
 show tables;
 desc accounts;
 select * from account_book;
+select * from accounts;
+select * from deposits;
+select * from expenses;
