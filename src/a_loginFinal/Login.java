66aa8896cf860ed565804854439d23727aea39bd
Seungwan Yu <dbtmddhks@gmail.com>
2023-11-22 02:38:36 +0900

    Revert "indent & UI fix"
    This reverts commit 3de6dd77bb30c1c6a6a5d8e5f970c39ce1a92b0e.

--- src/a_loginFinal/Login.java
+++ src/a_loginFinal/Login.java
@@ -14,27 +14,29 @@ import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+
 public class Login {
 	DB db = new DB();
 	SessionManager sm;
 	private JFrame f = new JFrame();
-
-	// 프로그램 시작점.
+	
+	//프로그램 시작점.
 	public Login() {
 		f.setLayout(null);
-
-		// 백그라운드 이미지
+		
+		//백그라운드 이미지
 		String img = "src/Images/Login5.png";
 		JLabel jl = new JLabel(new ImageIcon(img));
 		jl.setBounds(0, 0, 500, 728);
 		jl.setLayout(null);
-
+		
 		String calendar = "src/Images/calendar.png";
 		JLabel cal = new JLabel(new ImageIcon(calendar));
 		cal.setBounds(240, 240, 40, 46);
 		jl.add(cal);
-
-		// 로그인 라벨 & 텍스트필드
+		
+		
+		//로그인 라벨 & 텍스트필드
 		f.add(jl);
 		JTextField id = new JTextField(20);
 		JPasswordField pw = new JPasswordField(20);
@@ -43,58 +45,84 @@ public class Login {
 		JLabel PW = new JLabel("PW");
 		ID.setBounds(200, 300, 50, 50);
 		PW.setBounds(200, 330, 50, 50);
-
+		
 		id.setBounds(220, 315, 100, 23);
 		pw.setBounds(220, 342, 100, 23);
 		login.setBounds(320, 320, 50, 40);
 		login.addActionListener(new ActionListener() {
-
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String loginId = id.getText();
 				String loginPw = new String(pw.getPassword());
-				if (db.authenticateUser(loginId, loginPw)) {
+				if(db.authenticateUser(loginId, loginPw)) {
 					JOptionPane.showMessageDialog(null, "로그인 성공");
 					sm = SessionManager.getInstance();
-					sm.setCurrentUser(loginId);
+	                sm.setCurrentUser(loginId);
 					f.dispose();
 					MyProfile mp = new MyProfile(sm);
-				} else {
+				}else {
 					JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호를 찾을 수 없습니다.");
 				}
-
+				
 			}
 		});
-
+		
+		
+		
+		
 		ID.setForeground(Color.WHITE);
 		PW.setForeground(Color.WHITE);
-
+		
+		
 		jl.add(pw);
 		jl.add(PW);
 		jl.add(id);
 		jl.add(ID);
 		jl.add(login);
-
+		
+		
+		
+		
+		
+		
+		
 		JButton signUp = new JButton("회원가입");
 		signUp.setBounds(220, 600, 80, 40);
 		signUp.addActionListener(new ActionListener() {
-
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Sign_Up_Ui si = new Sign_Up_Ui();
 				si.signUp();
 				f.dispose();
-
+				
+				
 			}
 		});
 		jl.add(signUp);
-
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		f.setResizable(false);
 		f.setBounds(0, 0, 500, 728);
 		f.setLocationRelativeTo(null);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
-
+			
 	}
-
+	
+	
 }
+
--- src/accountBook1/PaymentTypeConverter.java
+++ src/accountBook1/PaymentTypeConverter.java
@@ -4,28 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PaymentTypeConverter {
-	private final Map<String, String> korToEngMap;
-	private final Map<String, String> engToKorMap;
+    private final Map<String, String> korToEngMap;
+    private final Map<String, String> engToKorMap;
 
-	public PaymentTypeConverter() {
-		korToEngMap = new HashMap<>();
-		// Add mappings for Korean payment types to English payment types
-		korToEngMap.put("현금", "cash");
+    public PaymentTypeConverter() {
+    	korToEngMap = new HashMap<>();
+        // Add mappings for Korean payment types to English payment types
+    	korToEngMap.put("현금", "cash");
 		korToEngMap.put("계좌1", "account1");
 		korToEngMap.put("계좌2", "account2");
 		korToEngMap.put("계좌3", "account3");
-
-		engToKorMap = new HashMap<>();
-		for (Map.Entry<String, String> entry : korToEngMap.entrySet()) {
-			engToKorMap.put(entry.getValue(), entry.getKey());
-		}
-	}
+		
+		
+		
+        engToKorMap = new HashMap<>();
+        for (Map.Entry<String, String> entry : korToEngMap.entrySet()) {
+            engToKorMap.put(entry.getValue(), entry.getKey());
+        }
+    }
 
 	public String convertKorToEngPayType(String korPaymentType) {
-		return korToEngMap.getOrDefault(korPaymentType, korPaymentType);
-	}
-
-	public String convertEngToKorPayType(String engPaymentType) {
-		return engToKorMap.getOrDefault(engPaymentType, engPaymentType);
-	}
+        return korToEngMap.getOrDefault(korPaymentType, korPaymentType);
+    }
+	
+    public String convertEngToKorPayType(String engPaymentType) {
+        return engToKorMap.getOrDefault(engPaymentType, engPaymentType);
+    }
 }
